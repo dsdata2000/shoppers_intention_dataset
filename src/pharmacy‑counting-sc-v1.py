c783cf880a09f8a42634e49a76176f07364aa3fa
dsGitHubRepository <dsGitHubRepository@users.noreply.github.com>
2018-07-15 21:17:44 -0400

    Update pharmacy‑counting-sc-v1.py

--- src/pharmacy‑counting-sc-v1.py
+++ src/pharmacy‑counting-sc-v1.py
@@ -1,3 +1,25 @@
+# NODE => N_of_data_entry; 500000
+NODE = int(round(500000/100))
+N_unit_test = 100
+N_start = int(round(0))   #  24525860/2, 24520000
+N = N_start + N_unit_test
+
+def index_list(list1,sorted_list):
+    #print len(list1), len(sorted_list)
+    index = []
+    for i in range(len(sorted_list)) :
+        ind = list1.index(sorted_list[i])
+        index.append(ind)
+    return index
+
+def count_frequency( list1, item1 ) :
+    s = 0
+    for i in range(len(list1)) :
+        if item1 == list1[i] :
+            s += 1
+    return s
+
 def test():
-    print('hello')
-print test()
+    print('h')
+test()
+    
