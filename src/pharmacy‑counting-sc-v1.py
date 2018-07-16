6aa256ca361b5989409dc3b9e3efe3d44b9faea8
dsGitHubRepository <dsGitHubRepository@users.noreply.github.com>
2018-07-15 20:14:08 -0400

    Update pharmacy‑counting-sc-v1.py

--- src/pharmacy‑counting-sc-v1.py
+++ src/pharmacy‑counting-sc-v1.py
@@ -3,6 +3,8 @@ def test():
     x = [10, 20, 30]
     for i in range( len(x) ) :
         cs = str(x[i])
-        print type(cs)        
+        print type(cs) 
+        output_data.write('%s\n' %(cs) )
     output_data.close()
+    
 test()
