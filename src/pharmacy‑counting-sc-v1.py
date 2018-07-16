8f1f67aba551053ec6b79eb30f0fc99e5830b260
dsGitHubRepository <dsGitHubRepository@users.noreply.github.com>
2018-07-15 20:06:27 -0400

    Update pharmacy‑counting-sc-v1.py

--- src/pharmacy‑counting-sc-v1.py
+++ src/pharmacy‑counting-sc-v1.py
@@ -1,4 +1,7 @@
 def test():
+    output_data = ('./output/top_cost_drug.txt','wb')
     x = [10, 20, 30]
-    return x 
+    for i in range( len(x) ) :
+        output_data.write('%s\n' % x[i])
+    output_data.close()
 test()
